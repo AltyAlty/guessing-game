3bae06ddbf0b6c76acc34b6b212db7036bf60d24
Alty <heneveraskedforthis@gmail.com>
2021-12-18 14:51:51 +0300

    my attempt

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,49 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        // this._greater = false;
+        // this._lower = false;
+
+        this._min = 0;
+        this._max = 0;
+        this._mid = 0;
+    };
 
     setRange(min, max) {
-
-    }
+        this._min = min;
+        this._max = max;
+    };
 
     guess() {
+        // if (this._lower !== true && this._greater !== true) {
+        //     this._mid = Math.ceil((this._max + this._min) / 2);
+        //     return this._mid;
+        // } else if (this._lower === true && this._greater !== true) {
+        //     this._mid = Math.ceil((this._max + this._min) / 2);
+        //     this._lower = false;
+        //     return this._mid;
+        // } else if (this._lower !== true && this._greater === true) {
+        //     this._mid = Math.ceil((this._max + this._min) / 2);
+        //     this._greater = false;
+        // };
 
-    }
+        this._mid = Math.ceil((this._max + this._min) / 2);
+
+        return this._mid;
+    };
 
     lower() {
+        // this._lower = true;
+        // this.guess();
 
-    }
+        this._max = this._mid;
+    };
 
     greater() {
+        // this._greater = true;
+        // this.guess();
 
-    }
-}
+        this._min = this._mid;
+    };
+};
 
 module.exports = GuessingGame;
